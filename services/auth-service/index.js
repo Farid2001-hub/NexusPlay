f83e766a907d1a0dd818b0e1401b0fecd5e1ca6b
hamza <hamzafarid556@.com>
2025-05-30 12:54:22 +0200

    expose metrique

--- services/auth-service/index.js
+++ services/auth-service/index.js
@@ -1,6 +1,20 @@
+// auth-service/index.js (ajoute ça)
+
 const express = require('express');
+const client = require('prom-client');
 require('dotenv').config();
+
 const app = express();
+
+// Create a Registry which registers the metrics
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+// Expose /metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 app.get('/auth', (req, res) => {
   res.send(`Auth OK - secret=${process.env.AUTH_SECRET}`);
--- services/game-service/index.js
+++ services/game-service/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const client = require('prom-client');
+
 const app = express();
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 app.get('/game', (req, res) => {
   res.send('Game Service OK');
 });
 
 app.listen(3002, () => console.log('Game Service on port 3002'));
-
-
